ebcf5205e2355db1ac30436ac3baee2db9272c30
Дмитрий <32355073+xex238@users.noreply.github.com>
2017-10-04 21:23:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class pivo
+{
+    public virtual void a ()
+    {
+        Console.WriteLine("Метод базового класса");
+    }
+}
+public class pivasic : pivo
+{
+    public override void  a()
+    {
+        Console.WriteLine("Метод производного класса");
+    }
+}
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pivasic q = new pivasic();
+            q.a();
+            pivo qq = new pivo();
+            qq.a();
+            pivo qqq = (pivo)q;
+            qqq.a();
+            pivo qqqq = new pivasic();
+            qqqq.a();
+        }
+    }
+}
