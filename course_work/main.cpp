28054856969b7b315d5ecb0836c7fbe340b665f6
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-01-08 21:16:11 +0300

    Update main.cpp

--- course_work/main.cpp
+++ course_work/main.cpp
@@ -1,68 +1,104 @@
 #include <SFML/Graphics.hpp>
+#include <SFML\Audio.hpp>
 #include "Player.h"
 #include "Map.h"
+#include "Menu.h"
+#include "Music.h"
 #include <string>
 using namespace sf;
 
 int main()
 {
-	bool b;
+	RenderWindow window(VideoMode(1366, 768), "New_Window", Style::Fullscreen, ContextSettings());
 
-	RenderWindow window(VideoMode(1000, 600), "New_Window", Style::Fullscreen);
+	Menu menu("pictures/main_menu.png", "pictures/arrow_back.png", "pictures/settings.png", "pictures/rectangle.png", "pictures/restart_level.png");
 
-	String s_hero = "C:/3_semester/SFML_go/sprite.png";
-	String s_map = "C:/3_semester/SFML_go/sprite_list.png";
+	Map map_1("pictures/sprite_list_textures.png", "pictures/sprite_list_arrows.png");
 
-	player car(s_hero, 1, 50, 135);
-	car.sp.setTextureRect(IntRect(0, 34, 40, 22));
+	Player car("pictures/sprite_list_cars.png", "pictures/sprite_list_arrows.png", map_1);
 
-	Map_ map_1(s_map);
+	Musics my_music("music/game_over.ogg", "music/fon_music.ogg");
+	my_music.Play_fon_music();
 
-	Clock clock;
+	Thread move_control_thread(&Player::Move_control, &car);
+
 	while (window.isOpen())
 	{
-		float time = clock.getElapsedTime().asMicroseconds();
-		clock.restart();
-		time = time / 800;
+		my_music.Replay();
+
+		car.Restart_time();
+
+		car.Update_pos(window);
+		menu.Update_pos(window);
 
 		Event event;
-		while (window.pollEvent(event))
+
+		while (window.pollEvent(event)) // <- Обработчик событий
 		{
 			if ((event.type == Event::Closed) || (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape))
 				window.close();
+			if (menu.Return_screen_text() == "left.png") // <- Если мы в игре
+			{
+				if (event.type == Event::KeyPressed && event.key.code == Keyboard::G)
+				{
+					car.Car_go(map_1, window, my_music);
+				}
+				//if (event.type == Event::KeyPressed && event.key.code == Keyboard::J)
+				//{
+				//	car.Go_to_vector_1(window);
+				//}
+				menu.Restart_level(event, car, map_1, my_music);
+				menu.Return_to_main_menu(event); // <- Если нажата кнопка назад
+				car.Player_MouseButtonPressed(event); // <- Если нажали на стрелочку и хотим её передвинуть
+				car.Player_MouseButtonReleased(event, map_1); // <- Если мы отпустили кнопку после передвижения стрелочки
+			}
+			if (menu.Return_screen_text() == "main.png") // <- Если мы в главном меню
+			{
+				menu.Menu_MouseButtonEvent(window, event);
+			}
+			if (menu.Return_screen_text() == "settings.png") // <- если мы в настройках
+			{
+				menu.Work_with_settings(event, my_music);
+			}
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Left))
+		if (menu.Return_screen_text() == "left.png") // <- Если мы в игре, обрабатываем движение машинки и стрелочки
 		{
-			car.sp.setTextureRect(IntRect(20, 0, 38, 24));
-			car.sp.move(-0.1*time, 0);
-			car.x += -0.1*time;
+			move_control_thread.launch(); // <- Обрабатываем движения машинки и стрелочки в новом потоке
 		}
 
-		else if (Keyboard::isKeyPressed(Keyboard::Right))
+		window.clear(Color::Cyan);
+
+		// <- Отрисовка графики
+		if (menu.Return_screen_text() == "left.png")  // <- Если мы в игре
 		{
-			car.sp.setTextureRect(IntRect(0, 34, 40, 22));
-			car.sp.move(0.1*time, 0);
-			car.x += 0.1*time;
+			map_1.Load_map(window);  // <- Рисуем карту
+			map_1.Draw_arrows(window); // <- Рисуем четыре стрелочки
+			car.Draw_arrows(window);  // <- Рисуем все стрелочки с отдельными спрайтами
+			window.draw(car.Return_sprite_cars());  // <- Рисуем спрайт машинки
+			window.draw(menu.Return_left_sprite());  // <- Рисуем спрайт кнопочки "назад"
+			window.draw(menu.Return_restart_level_sprite()); // <- Рисуем кнопку перезапуска уровня
+			if (car.Return_enable_text_for_win())
+			{
+				window.draw(car.Return_text_for_win());
+			}
 		}
-
-		else if (Keyboard::isKeyPressed(Keyboard::Down))
+		if (menu.Return_screen_text() == "main.png")  // <- Если мы в главном меню
 		{
-			car.sp.setTextureRect(IntRect(0, 0, 22, 36));
-			car.sp.move(0, 0.1*time);
-			car.y += 0.1*time;
+			window.draw(menu.Return_main_sprite());
+			if (menu.Return_enable_rectangle_sprite())
+			{
+				window.draw(menu.Return_rectangle_sprite());
+			}
 		}
-
-		else if (Keyboard::isKeyPressed(Keyboard::Up))
+		if (menu.Return_screen_text() == "settings.png")  // <- Если мы в настройках
 		{
-			car.sp.setTextureRect(IntRect(56, 0, 22, 40));
-			car.sp.move(0, -0.1 * time);
-			car.y += -0.1*time;
+			window.draw(menu.Return_settings_sprite());
+			window.draw(menu.Return_left_sprite());
+			window.draw(menu.Return_text());
+			window.draw(menu.Return_sounds_enable_list_sprite());
 		}
 
-		window.clear();
-		map_1.Load_map(window);
-		window.draw(car.sp);
 		window.display();
 	}
 }
