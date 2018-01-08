8ec26342a665139169e2bbf169e4f117ee3dcb75
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-01-08 21:28:22 +0300

    Create Music.h

---
+++ course_work/Music.h
@@ -0,0 +1,38 @@
+#include <SFML/Graphics.hpp>
+#include <SFML\Audio.hpp>
+using namespace sf;
+#ifndef The_best_music
+#define The_best_music
+
+class Musics
+{
+public:
+	Musics() = default;
+
+	Musics(const String& s_SoundBuffer, const String& s_Music);
+
+	~Musics() = default;
+
+	void Replay();
+
+	void Play_fon_music();
+
+	void Play_game_over_music();
+
+	void Fon_music_set_volume(const float& x);
+
+	float Fon_music_get_volume();
+
+	void Stop_fon_music();
+
+	void Stop_game_over_music();
+
+	void Game_over_music_set_volume(const float& x);
+
+private:
+	Music game_over_music;   // <- this is music for game over
+	Music fon_music;   // <- this is fon music
+};
+
+#endif // !Music
+
