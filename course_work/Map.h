12aef075ff7071a2f6e36a2c3deec73b1a578986
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-01-08 21:17:41 +0300

    Update Map.h

--- course_work/Map.h
+++ course_work/Map.h
@@ -1,21 +1,23 @@
-#ifndef Map
-#define Map
+#ifndef Map_
+#define Map_
 #include <SFML/Graphics.hpp>
 using namespace sf;
+#include "Arrow.h"
+#include "Massiv_arrows.h"
 #include <string>
 
-class Map_
+class Map
 {
-	const int H = 12;
-	const int W = 30;
+public:
+	String** title_map_; // <- Копия исходной карты
 
-	String title_map[12] =
+	const String title_map_1[12] =
 	{
 		"NNNNNNNNNNNNNNNNNNNNNNNNNNNNN",
 		"N                           N",
 		"N       JWWWWWWWWJ          N",
 		"N       W        W          N",
-		"NWWWWWWWJ        W          N",
+		"NBWWWWWWJ        W          N",
 		"N                W          N",
 		"N                W          N",
 		"N                JWWWWWX    N",
@@ -24,14 +26,37 @@ class Map_
 		"N                           N",
 		"NNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
 	};
-public:
-	Image map_image;
+
+	~Map() = default;
+
+	Map(const String& string_for_load_map_texture, const String& string_for_load_arrows_texture);
+
+	void Draw_arrows(RenderWindow& window);
+
+	void Load_map(RenderWindow& window);
+
+	Texture Return_map_texture();
+
+	int Return_H();
+
+	int Return_W();
+
+	int Return_vector();
+
+	Vector2i Return_burn();
+private:
+	const int H = 12;
+	const int W = 30;
+
+	int vector{ 0 };
+
+	Vector2i burn;
+
 	Texture map_texture;
 	Sprite map_sprite;
 
-	Map_(String s);
-
-	void Load_map(RenderWindow& window);
+	Texture arrows_texture;
+	Sprite arrows_sprite;
 };
 
 #endif // !Map
