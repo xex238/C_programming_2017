6f3bd855477c816433838938317a229b70b7e006
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-01-29 09:35:55 +0300

    Update Player.cpp

--- course_work/Player.cpp
+++ course_work/Player.cpp
@@ -1,18 +1,23 @@
 #include "Player.h"
 
-Player::Player(const int x)
+Player::Player()
 {
 	my_music.Play_fon_music();
+	bool load_level = false;
+	load_level = level_1.LoadFromFile("maps/Map_for_game_1.tmx"); // Загружаем карту
+	level_1.Dislocate(64, 128); // Перемещаем карту
+	level_1.Dislocate_objects(64, 128); // Перемещаем объекты
+	car.Load_car(level_1);
 }
 
-void Player::Refresh(RenderWindow& window)
+void Player::Refresh(const RenderWindow& window)
 {
 	my_music.Replay();
 	car.Restart_time();
 	Update_pos(window);
 }
 
-void Player::Update_pos(RenderWindow & window)
+void Player::Update_pos(const RenderWindow& window)
 {
 	pixel_pos = Mouse::getPosition(window);
 	pos = window.mapPixelToCoords(pixel_pos);
@@ -26,12 +31,12 @@ void Player::Game_event(RenderWindow& window, Event& event)
 	{
 		if (event.type == Event::KeyPressed && event.key.code == Keyboard::G)
 		{
-			car.Car_go(map_1, window, my_music);
+			car.Car_go(/*window, */my_music);
 		}
-		menu.Restart_level(event, car, map_1, my_music, pos);
+		menu.Restart_level(event, car, my_music, pos);
 		menu.Return_to_main_menu(event, pos); // <- Если нажата кнопка назад
 		car.Player_MouseButtonPressed(event, pos); // <- Если нажали на стрелочку и хотим её передвинуть
-		car.Player_MouseButtonReleased(event, map_1, pos); // <- Если мы отпустили кнопку после передвижения стрелочки
+		car.Player_MouseButtonReleased(event, pos); // <- Если мы отпустили кнопку после передвижения стрелочки
 	}
 	if (menu.Return_screen_text() == "main.png") // <- Если мы в главном меню
 	{
@@ -47,9 +52,10 @@ void Player::Draw_graphics(RenderWindow& window)
 {
 	if (menu.Return_screen_text() == "left.png")  // <- Если мы в игре
 	{
-		map_1.Load_map(window);  // <- Рисуем карту
-		map_1.Draw_arrows(window); // <- Рисуем четыре стрелочки
-		car.Draw_arrows(window);  // <- Рисуем все стрелочки с отдельными спрайтами
+		//map_1.Load_map(window);  // <- Рисуем старую карту
+		level_1.Draw(window); // <- Рисуем новую карту
+		car.Draw_arrows(window); // <- Рисуем четыре стрелочки
+		car.Draw_additional_arrows(window);  // <- Рисуем все стрелочки с отдельными спрайтами
 		window.draw(car.Return_sprite_cars());  // <- Рисуем спрайт машинки
 		window.draw(menu.Return_left_sprite());  // <- Рисуем спрайт кнопочки "назад"
 		window.draw(menu.Return_restart_level_sprite()); // <- Рисуем кнопку перезапуска уровня
@@ -82,9 +88,3 @@ void Player::Move_car_on_rows()
 		car.Move_control(pos);
 	}
 }
-
-//void Player::Music_on_victory()
-//{
-//	my_music.Stop_fon_music();
-//	my_music.Play_game_over_music();
-//}
