7d027d7eced7e93f9634390a6770b4669acf63dd
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-01-29 09:25:32 +0300

    Add files via upload

---
+++ course_work/maps/tileset_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_1" tilewidth="32" tileheight="32" tilecount="480" columns="30">
+ <image source="../pictures/sprite_list_textures.png" width="960" height="512"/>
+</tileset>
