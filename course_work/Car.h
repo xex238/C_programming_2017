14189cdbbb02e74d4d9fb9b5b49a67bacff3b417
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-01-29 09:32:53 +0300

    Update Car.h

--- course_work/Car.h
+++ course_work/Car.h
@@ -1,42 +1,55 @@
 #include <SFML/Graphics.hpp>
-#include "Map.h"
 #include "Music.h"
+#include "Level.h"
+#include <vector>
+
 #ifndef Car_
 #define Car_
 
 class Car
 {
 public:
-	Car(Map& map_1);
+	Car();
 
 	~Car() = default;
 
-	void Move_control(Vector2f pos);
+	void Load_car(Level& lev);
 
-	void Go_without_time(const int& vector, RenderWindow& window);
+	void Move_control(const Vector2f& pos);
+
+	void Go_without_time(const int& vector/*, RenderWindow& window*/);
 
 	void Go_with_time(const int& vector);
 
-	void Player_MouseButtonPressed(const Event& event, Vector2f pos);
+	void Player_MouseButtonPressed(const Event& event, const Vector2f& pos);
 
-	void Player_MouseButtonReleased(const Event& event, const Map& map_1, Vector2f pos);
+	void Player_MouseButtonReleased(const Event& event, const Vector2f& pos);
 
 	void Restart_time();
 
+	void Draw_additional_arrows(RenderWindow& window);
+
 	void Draw_arrows(RenderWindow& window);
 
-	void Car_go(const Map& map_1, RenderWindow& window, Musics& my_music);
+	void Car_go(/*RenderWindow& window, */Musics& my_music);
 
 	Sprite Return_sprite_cars();
 
-	//void Go_to_vector_1(RenderWindow& window);
-
-	void Restart_level(Map& map_1, Musics& my_music);
+	void Restart_level(Musics& my_music);
 
 	bool Return_enable_text_for_win();
 
 	Text Return_text_for_win();
 
+	String Check_collision_with_plate(const Vector2f& pos, const String& number_arrow);
+
+	bool Check_collision_with_arrows_for_car_go(const float& x, const float& y);
+
+	bool Check_collision_with_exit_for_car_go(const float& x, const float& y);
+
+	bool Check_on_true_vector(const float& x, const float& y);
+
+	void Pick_texture_for_car();
 private:
 	Texture texture_cars;
 	Sprite sprite_cars;
@@ -47,26 +60,32 @@ private:
 	Texture arrows_texture;
 	Sprite arrows_sprite;
 
-	Clock clock;
+	Clock clock; // Переменная времени
 	float time;
 
 	float dx_car{ 0 }; // <- х-координата при передвижении машинки
 	float dy_car{ 0 }; // <- у-координата при передвижении машинки
 
-	Sprite* arrows;
+	Sprite* arrows; // <- Массив спрайтов для создания новых стрелочек при их перемещении на таблички
 	int capacity_arrows{ 0 };  // <- Размер массива спрайтов
 	int size_arrows{ -1 }; // <- Количество текущих значений в массиве (количество спрайтов)
 	int number_of_move_arrow{ 0 };  // <- Номер текущего передвигаемого спрайта
-	String vector_arrows_string{ "" };
-	int vector_of_arrows_int{ 0 };
+	String vector_arrows_string{ "" }; // <- Хранение циферки для таблички на карте
 	float dx_number_of_arrow{ 0 };  // <- х-позиция текущего передвигаемого спрайта
 	float dy_number_of_arrow{ 0 };  // <- у-позиция текущего передвигаемого спрайта
 	bool arrow_is_move = false;  // <- Передвигаем ли мы спрайт
-	bool in_this_rectangle_has_arrow = false;  // Есть ли на позиции зажатой мыши спрайт
 
 	Font font_for_text_for_win;
 	Text text_for_win = Text("You are win!!!", font_for_text_for_win, 128);
 	bool enable_text_for_win = false;
+
+	Object burn; // Объект точки рождения
+	int burn_vector; // Вектор рождения
+
+	Object exit; // Объект точки выхода
+
+	std::vector<Object> obj; // Вектор объектов карты
+	std::vector<Object> vector_of_right_direction; // Вектор первоначальных объектов с правильными направлениями
 };
 
 #endif // !Car
