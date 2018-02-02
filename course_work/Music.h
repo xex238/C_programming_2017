1555595752dd2869e87147d4b33b7becedb117f8
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-02-02 13:09:27 +0300

    Update Music.h

--- course_work/Music.h
+++ course_work/Music.h
@@ -8,25 +8,17 @@ class Musics
 {
 public:
 	Musics();
-
 	~Musics() = default;
 
 	void Replay();
-
 	void Play_fon_music();
-
 	void Play_game_over_music();
-
-	void Fon_music_set_volume(const float& x);
-
-	float Fon_music_get_volume();
-
 	void Stop_fon_music();
-
 	void Stop_game_over_music();
-
+	void Fon_music_set_volume(const float& x);
 	void Game_over_music_set_volume(const float& x);
 
+	float Fon_music_get_volume();
 private:
 	Music game_over_music;   // <- Это музыка будет играть при победе
 	Music fon_music;   // <- Это фоновая музыка
