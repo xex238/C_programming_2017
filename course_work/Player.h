9b7828c4f0b90b818f56f7d84c1ef973a33f4918
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-02-02 13:10:49 +0300

    Update Player.h

--- course_work/Player.h
+++ course_work/Player.h
@@ -12,26 +12,29 @@ class Player
 {
 public:
 	Player();
-
 	~Player() = default;
 
 	void Refresh(const RenderWindow& window);
-
 	void Update_pos(const RenderWindow& window);
-
 	void Game_event(RenderWindow& window, Event& event);
-
 	void Draw_graphics(RenderWindow& window);
-
-	void Move_car_on_rows();
+	void Car_go(RenderWindow& window);
+	void Choose_level_for_load(Event& event);
+	void Load_level(Level& level_, const String& string_level);
 private:
 	Level level_1; // <- Переменная уровня
+	Level level_2;
+	Level level_3;
+	Level level_4;
+	Level level_5;
 
-	Menu menu; // <- Переменная меню
+	int level{ 0 };
+
+	Menu menu; // <- Переменная меню (Инициализируется сразу)
 
 	Car car; // <- Переменная машинки
 
-	Musics my_music; // <- Переменная музыки
+	Musics my_music; // <- Переменная музыки (инициализируется сразу)
 
 	Vector2i pixel_pos; // <- Координаты мыши в пикселях
 	Vector2f pos; // <- Координаты мыши
