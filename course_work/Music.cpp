62d2ab3e446906a679900b8ea0827394cce69e1d
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-02-02 13:08:59 +0300

    Update Music.cpp

--- course_work/Music.cpp
+++ course_work/Music.cpp
@@ -12,38 +12,32 @@ void Musics::Replay() // <- Включение повторения музыки
 {
 	fon_music.setLoop(true);
 }
-
 void Musics::Play_fon_music()
 {
 	fon_music.play();
 }
-
 void Musics::Play_game_over_music()
 {
 	game_over_music.play();
 }
-
+void Musics::Stop_fon_music()
+{
+	fon_music.stop();
+}
+void Musics::Stop_game_over_music()
+{
+	game_over_music.stop();
+}
 void Musics::Fon_music_set_volume(const float& x)
 {
 	fon_music.setVolume(x);
+}
+void Musics::Game_over_music_set_volume(const float& x)
+{
+	game_over_music.setVolume(x);
 }
 
 float Musics::Fon_music_get_volume()
 {
 	return fon_music.getVolume();
-}
-
-void Musics::Stop_fon_music()
-{
-	fon_music.stop();
-}
-
-void Musics::Stop_game_over_music()
-{
-	game_over_music.stop();
-}
-
-void Musics::Game_over_music_set_volume(const float& x)
-{
-	game_over_music.setVolume(x);
 }
