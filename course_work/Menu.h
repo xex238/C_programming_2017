7db821875284c42b86c1316b5e50f9bb085df0ec
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-02-02 13:08:30 +0300

    Update Menu.h

--- course_work/Menu.h
+++ course_work/Menu.h
@@ -8,38 +8,30 @@ class Menu
 {
 public:
 	Menu();
-
 	~Menu() = default;
 
 	void Menu_MouseButtonReleased_left(RenderWindow& window, const Vector2f& pos);
-
 	void Menu_MouseButtonPressed_left(const RenderWindow& window, const Vector2f& pos);
-
 	void Menu_MouseButtonEvent(RenderWindow& window, const Event& event, const Vector2f& pos);
-
 	void Work_with_settings(const Event& event, Musics& my_music, const Vector2f& pos);
-
-	void Return_to_main_menu(const Event& event, const Vector2f& pos);
-
+	void Return_to_main_menu(const Event& event, const Vector2f& pos, Car& car, Level& level, Musics& my_music);
 	void Restart_level(const Event& event, Car& car, Musics& my_music, const Vector2f& pos);
+	void Set_screen_text(const String& screen_text_);
+	void Set_play_game_over_music(const bool& boolean);
+
+	bool Return_enable_rectangle_sprite();
 
 	Text Return_text();
 
 	String Return_screen_text();
 
 	Sprite Return_sounds_enable_list_sprite();
-
 	Sprite Return_main_sprite();
-
 	Sprite Return_left_sprite();
-
 	Sprite Return_settings_sprite();
-
 	Sprite Return_rectangle_sprite();
-
 	Sprite Return_restart_level_sprite();
-
-	bool Return_enable_rectangle_sprite();
+	Sprite Return_level_screen_sprite();
 private:
 	Texture restart_level_texture; // <- Для кнопки перезагрузки уровня
 	Sprite restart_level_sprite;
@@ -55,11 +47,14 @@ private:
 
 	Texture rectangle_texture; // <- Для подсветки, когда нажата клавиша
 	Sprite rectangle_sprite;
-	bool enable_rectangle_sprite = false;
+	bool enable_rectangle_sprite{ false };
 
 	Texture sounds_enable_list_texture; // <- Текстурка значка включенного/выключенного звука
 	Sprite sounds_enable_list_sprite;
-	bool sounds_enable = true; // <- Включен или выключен звук
+	bool sounds_enable = { true }; // <- Включен или выключен звук
+
+	Texture level_screen_texture;
+	Sprite level_screen_sprite;
 
 	Font font;  // <- Шрифт для текста
 	Text text = Text("", font, 20); // <- Сам текст
@@ -68,7 +63,7 @@ private:
 
 	float sound_bufer_volume{ 0 }; // <- Для хранения громкости звука
 
-	int text_int_real[3]{ 1, 0, 0 }; // <- Массив для перевода значений из типа int в тип char*
+	bool play_game_over_music{ false }; // Играет ли победная музыка
 };
 
 #endif // !menu
